88b0a98bc2df1ac8f71d0fd721d40867e2145687
Matanofri5 <45266810+Matanofri5@users.noreply.github.com>
2019-07-06 18:06:53 +0300

    start spring !

---
+++ src/main/java/com/spring/beans/Company.java
@@ -0,0 +1,35 @@
+package com.spring.beans;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Basic(optional = false)
+	@Column(nullable = false)
+	private String companyName;
+	
+	@Basic(optional = false)
+	@Column(nullable = false)
+	private String password;
+	
+	@Basic(optional = false)
+	@Column(nullable = false)
+	private String email;
+
+	
+}
